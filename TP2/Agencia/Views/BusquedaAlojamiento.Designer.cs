450c3d9e0c61b9b126de838126b0b2ce1b35e47f
Gianpoolmantilla <gpmantilla@gmail.com>
2021-05-15 00:01:51 -0300

    modificacion

--- TP2/Agencia/Views/BusquedaAlojamiento.Designer.cs
+++ TP2/Agencia/Views/BusquedaAlojamiento.Designer.cs
@@ -29,31 +29,32 @@ namespace Agencia.Views
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.text_fechah = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.btnLogin = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
-            this.seleccion = new System.Windows.Forms.ComboBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.seleccion_tipo = new System.Windows.Forms.ComboBox();
+            this.text_ciudad = new System.Windows.Forms.TextBox();
+            this.text_cantidad = new System.Windows.Forms.TextBox();
+            this.text_fechad = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox4
+            // text_fechah
             // 
-            this.textBox4.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBox4.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(401, 151);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(81, 31);
-            this.textBox4.TabIndex = 55;
+            this.text_fechah.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.text_fechah.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_fechah.Location = new System.Drawing.Point(266, 152);
+            this.text_fechah.Name = "text_fechah";
+            this.text_fechah.Size = new System.Drawing.Size(81, 31);
+            this.text_fechah.TabIndex = 55;
             // 
             // label5
             // 
@@ -73,11 +74,12 @@ namespace Agencia.Views
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Verdana", 13.25F);
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(113, 265);
+            this.label6.Location = new System.Drawing.Point(17, 60);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(189, 22);
+            this.label6.Size = new System.Drawing.Size(48, 22);
             this.label6.TabIndex = 53;
-            this.label6.Text = "Tipo de alojamiento";
+            this.label6.Text = "Tipo";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label1
             // 
@@ -85,23 +87,11 @@ namespace Agencia.Views
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Verdana", 13.25F);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(113, 97);
+            this.label1.Location = new System.Drawing.Point(7, 128);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(72, 22);
             this.label1.TabIndex = 45;
             this.label1.Text = "Ciudad";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.Font = new System.Drawing.Font("Verdana", 13.25F);
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(113, 155);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(62, 22);
-            this.label3.TabIndex = 46;
-            this.label3.Text = "Fecha";
             // 
             // btnLogin
             // 
@@ -112,7 +102,7 @@ namespace Agencia.Views
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogin.Font = new System.Drawing.Font("Cambria", 13.75F, System.Drawing.FontStyle.Bold);
             this.btnLogin.ForeColor = System.Drawing.Color.White;
-            this.btnLogin.Location = new System.Drawing.Point(365, 324);
+            this.btnLogin.Location = new System.Drawing.Point(483, 148);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(117, 39);
             this.btnLogin.TabIndex = 48;
@@ -126,51 +116,51 @@ namespace Agencia.Views
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Verdana", 13.25F);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(113, 214);
+            this.label4.Location = new System.Drawing.Point(367, 128);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(197, 22);
+            this.label4.Size = new System.Drawing.Size(90, 22);
             this.label4.TabIndex = 47;
-            this.label4.Text = "Numero de personas";
+            this.label4.Text = "Cantidad";
             // 
-            // seleccion
+            // seleccion_tipo
             // 
-            this.seleccion.BackColor = System.Drawing.Color.AliceBlue;
-            this.seleccion.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.seleccion.FormattingEnabled = true;
-            this.seleccion.Items.AddRange(new object[] {
+            this.seleccion_tipo.BackColor = System.Drawing.Color.AliceBlue;
+            this.seleccion_tipo.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.seleccion_tipo.FormattingEnabled = true;
+            this.seleccion_tipo.Items.AddRange(new object[] {
             "Hotel",
             "Cabañas"});
-            this.seleccion.Location = new System.Drawing.Point(351, 261);
-            this.seleccion.Name = "seleccion";
-            this.seleccion.Size = new System.Drawing.Size(130, 31);
-            this.seleccion.TabIndex = 52;
+            this.seleccion_tipo.Location = new System.Drawing.Point(11, 85);
+            this.seleccion_tipo.Name = "seleccion_tipo";
+            this.seleccion_tipo.Size = new System.Drawing.Size(130, 31);
+            this.seleccion_tipo.TabIndex = 52;
             // 
-            // textBox3
+            // text_ciudad
             // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBox3.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(276, 94);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(206, 31);
-            this.textBox3.TabIndex = 49;
+            this.text_ciudad.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.text_ciudad.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_ciudad.Location = new System.Drawing.Point(11, 153);
+            this.text_ciudad.Name = "text_ciudad";
+            this.text_ciudad.Size = new System.Drawing.Size(130, 31);
+            this.text_ciudad.TabIndex = 49;
             // 
-            // textBox2
+            // text_cantidad
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBox2.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(351, 210);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(130, 31);
-            this.textBox2.TabIndex = 51;
+            this.text_cantidad.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.text_cantidad.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_cantidad.Location = new System.Drawing.Point(364, 153);
+            this.text_cantidad.Name = "text_cantidad";
+            this.text_cantidad.Size = new System.Drawing.Size(93, 31);
+            this.text_cantidad.TabIndex = 51;
             // 
-            // textBox1
+            // text_fechad
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.textBox1.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(276, 152);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(81, 31);
-            this.textBox1.TabIndex = 50;
+            this.text_fechad.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.text_fechad.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_fechad.Location = new System.Drawing.Point(159, 153);
+            this.text_fechad.Name = "text_fechad";
+            this.text_fechad.Size = new System.Drawing.Size(81, 31);
+            this.text_fechad.TabIndex = 50;
             // 
             // label7
             // 
@@ -178,7 +168,7 @@ namespace Agencia.Views
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Verdana", 9.25F);
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(358, 166);
+            this.label7.Location = new System.Drawing.Point(280, 134);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(46, 16);
             this.label7.TabIndex = 56;
@@ -190,7 +180,7 @@ namespace Agencia.Views
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Verdana", 9.25F);
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(228, 166);
+            this.label8.Location = new System.Drawing.Point(166, 134);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(48, 16);
             this.label8.TabIndex = 57;
@@ -198,23 +188,32 @@ namespace Agencia.Views
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.text_fechah);
             this.panel1.Controls.Add(this.btnLogin);
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label8);
             this.panel1.Controls.Add(this.label6);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.text_fechad);
+            this.panel1.Controls.Add(this.text_cantidad);
+            this.panel1.Controls.Add(this.text_ciudad);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.seleccion);
+            this.panel1.Controls.Add(this.seleccion_tipo);
             this.panel1.Location = new System.Drawing.Point(28, 30);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(618, 389);
             this.panel1.TabIndex = 66;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(21, 211);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(579, 154);
+            this.dataGridView1.TabIndex = 58;
             // 
             // BusquedaAlojamiento
             // 
@@ -230,25 +229,26 @@ namespace Agencia.Views
             this.Load += new System.EventHandler(this.BusquedaAlojamiento_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox text_fechah;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnLogin;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox seleccion;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox seleccion_tipo;
+        private System.Windows.Forms.TextBox text_ciudad;
+        private System.Windows.Forms.TextBox text_cantidad;
+        private System.Windows.Forms.TextBox text_fechad;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- TP2/Agencia/Views/BusquedaAlojamiento.cs
+++ TP2/Agencia/Views/BusquedaAlojamiento.cs
@@ -1,77 +1,83 @@
-﻿using Bussines;
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using Bussines;
 
 namespace Agencia.Views
 {
-    public partial class Cliente : Form
+    public partial class BusquedaAlojamiento : Form
     {
-        //AgenciaManager Ag = new AgenciaManager();
-
-        Bussines.Alojamiento al = new Bussines.Hotel("Ciudad", "barrio", "***", 5, true, 5454545);
-        Bussines.Agencia a = new Bussines.Agencia();
-
-        public Cliente()
+        AgenciaManager Ag = new AgenciaManager();
+        public BusquedaAlojamiento()
         {
             InitializeComponent();
-           // mesajeError.Visible = false;
+            panel1.BackColor = Color.FromArgb(60, Color.Black);
         }
-
-        private void pictureBox3_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void btnCerrar_Click(object sender, EventArgs e)
-        {
-
-            Application.Exit();
-        }
-
-
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-        //    if (!string.IsNullOrEmpty(text_localidad.Text) && !string.IsNullOrEmpty(text_fecha.Text) && !string.IsNullOrEmpty(text_fecha.Text)
-        //        && !string.IsNullOrEmpty(text_cantidad.Text) && !string.IsNullOrEmpty(seleccion_tipo_aloj.Text))
-        //    {
+            if (!string.IsNullOrEmpty(text_ciudad.Text) && !string.IsNullOrEmpty(text_fechad.Text) && !string.IsNullOrEmpty(text_fechah.Text)
+              && !string.IsNullOrEmpty(text_cantidad.Text) && !string.IsNullOrEmpty(seleccion_tipo.Text))
+            {
 
-        //        a.insertarAlojamiento(al);
-        //        AgenciaManager Ag = new AgenciaManager(a);
-
-        //        var Lista = Ag.buscarAlojamientos(text_localidad.Text, DateTime.Parse(text_fecha.Text),
-        //                                     DateTime.Parse(text_fecha.Text), int.Parse(text_cantidad.Text), seleccion_tipo_aloj.Text);
+                var Lista = Ag.buscarAlojamientos(text_ciudad.Text, DateTime.Parse(text_fechad.Text),
+                                             DateTime.Parse(text_fechah.Text), int.Parse(text_cantidad.Text), seleccion_tipo.Text);
 
 
-        //        for (int i = 0; i < Lista.Count; i++)
-        //        {
-        //            //adicionamos un row
-        //            dataGridView1.Rows.Add();
-        //            //colocamos la info
-        //            dataGridView1.Rows[i].Cells[0].Value = Lista.ToString();
-        //        }
+                for (int i = 0; i < Lista.Count; i++)
+                {
+                    //adicionamos un row
+                    dataGridView1.Rows.Add();
+                    //colocamos la info
+                    dataGridView1.Rows[i].Cells[0].Value = Lista.ToString();
+                }
 
-        //        //limpío los campos de los filtros
+               
 
-        //        text_localidad.Text = "";
-        //        text_fecha.Text = "";
-        //        text_fecha.Text = "";
-        //        text_cantidad.Text = "";
-        //        seleccion_tipo_aloj.Text = "";
+                //limpío los campos de los filtros
 
-        //    }
-        //    else
-        //    {
-        //        mesajeError.Visible = true;
-        //        mesajeError.Text = "Todos los campos son requeridos!!";
+                //text_localidad.Text = "";
+                //text_fecha.Text = "";
+                //text_fecha.Text = "";
+                //text_cantidad.Text = "";
+                //seleccion_tipo_aloj.Text = "";
+            }
+            else
+            {
+                //mesajeError.Visible = true;
+                //mesajeError.Text = "Todos los campos son requeridos!!";
 
-        //    }
+            }
 
+            this.Hide();
+            ResultadoBusqueda rb = new ResultadoBusqueda();
+            rb.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
 
         }
 
+        private void button6_Click(object sender, EventArgs e)
+        {
 
+        }
 
+        private void BusquedaAlojamiento_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
