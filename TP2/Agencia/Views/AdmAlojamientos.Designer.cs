6fe53509bea2397eb335d86ee500e66ed281e2ee
Gianpoolmantilla <gpmantilla@gmail.com>
2021-05-16 23:46:16 -0300

    alojamiento

--- TP2/Agencia/Views/AdmAlojamientos.Designer.cs
+++ TP2/Agencia/Views/AdmAlojamientos.Designer.cs
@@ -34,10 +34,16 @@ namespace Agencia.Views
             this.label3 = new System.Windows.Forms.Label();
             this.ciudadText = new System.Windows.Forms.TextBox();
             this.alojamientosGrid = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tipo_text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Barrio_text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NroHabitacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ciudad_text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estrellas_text = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Editar = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Eliminar = new System.Windows.Forms.DataGridViewButtonColumn();
             this.barrioText = new System.Windows.Forms.TextBox();
-            this.button5 = new System.Windows.Forms.Button();
             this.estrellasText = new System.Windows.Forms.TextBox();
-            this.button6 = new System.Windows.Forms.Button();
             this.tipoAlojamientoCombo = new System.Windows.Forms.ComboBox();
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -46,7 +52,6 @@ namespace Agencia.Views
             this.habitacionesText = new System.Windows.Forms.TextBox();
             this.precioText = new System.Windows.Forms.TextBox();
             this.personasText = new System.Windows.Forms.TextBox();
-            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.alojamientosGrid)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -56,7 +61,7 @@ namespace Agencia.Views
             this.tvNo.AutoSize = true;
             this.tvNo.Font = new System.Drawing.Font("Verdana", 8.25F);
             this.tvNo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.tvNo.Location = new System.Drawing.Point(99, 261);
+            this.tvNo.Location = new System.Drawing.Point(460, 88);
             this.tvNo.Margin = new System.Windows.Forms.Padding(0);
             this.tvNo.Name = "tvNo";
             this.tvNo.Size = new System.Drawing.Size(40, 17);
@@ -70,7 +75,7 @@ namespace Agencia.Views
             this.tvSi.AutoSize = true;
             this.tvSi.Font = new System.Drawing.Font("Verdana", 8.25F);
             this.tvSi.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.tvSi.Location = new System.Drawing.Point(32, 261);
+            this.tvSi.Location = new System.Drawing.Point(415, 88);
             this.tvSi.Margin = new System.Windows.Forms.Padding(0);
             this.tvSi.Name = "tvSi";
             this.tvSi.Size = new System.Drawing.Size(36, 17);
@@ -84,7 +89,7 @@ namespace Agencia.Views
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Verdana", 10.25F);
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(29, 289);
+            this.label3.Location = new System.Drawing.Point(293, 87);
             this.label3.Margin = new System.Windows.Forms.Padding(0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(113, 17);
@@ -97,7 +102,7 @@ namespace Agencia.Views
             this.ciudadText.BackColor = System.Drawing.SystemColors.ControlText;
             this.ciudadText.Font = new System.Drawing.Font("Cambria", 11F);
             this.ciudadText.ForeColor = System.Drawing.SystemColors.Menu;
-            this.ciudadText.Location = new System.Drawing.Point(17, 73);
+            this.ciudadText.Location = new System.Drawing.Point(17, 48);
             this.ciudadText.Name = "ciudadText";
             this.ciudadText.Size = new System.Drawing.Size(137, 25);
             this.ciudadText.TabIndex = 56;
@@ -109,67 +114,89 @@ namespace Agencia.Views
             this.alojamientosGrid.BackgroundColor = System.Drawing.SystemColors.WindowFrame;
             this.alojamientosGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.alojamientosGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Index});
-            this.alojamientosGrid.Location = new System.Drawing.Point(306, 73);
+            this.Id,
+            this.Tipo_text,
+            this.Barrio_text,
+            this.NroHabitacion,
+            this.Ciudad_text,
+            this.Estrellas_text,
+            this.Editar,
+            this.Eliminar});
+            this.alojamientosGrid.Location = new System.Drawing.Point(17, 210);
             this.alojamientosGrid.Name = "alojamientosGrid";
-            this.alojamientosGrid.Size = new System.Drawing.Size(286, 232);
+            this.alojamientosGrid.Size = new System.Drawing.Size(587, 160);
             this.alojamientosGrid.TabIndex = 59;
             this.alojamientosGrid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            this.Id.Width = 50;
+            // 
+            // Tipo_text
+            // 
+            this.Tipo_text.HeaderText = "Tipo";
+            this.Tipo_text.Name = "Tipo_text";
+            this.Tipo_text.Width = 50;
+            // 
+            // Barrio_text
+            // 
+            this.Barrio_text.HeaderText = "Barrio";
+            this.Barrio_text.Name = "Barrio_text";
+            this.Barrio_text.Width = 50;
+            // 
+            // NroHabitacion
+            // 
+            this.NroHabitacion.HeaderText = "Nro Habitacion";
+            this.NroHabitacion.Name = "NroHabitacion";
+            this.NroHabitacion.Width = 50;
+            // 
+            // Ciudad_text
+            // 
+            this.Ciudad_text.HeaderText = "Ciudad";
+            this.Ciudad_text.Name = "Ciudad_text";
+            this.Ciudad_text.Width = 50;
+            // 
+            // Estrellas_text
+            // 
+            this.Estrellas_text.HeaderText = "Estrella";
+            this.Estrellas_text.Name = "Estrellas_text";
+            this.Estrellas_text.Width = 50;
+            // 
+            // Editar
+            // 
+            this.Editar.HeaderText = "Editar";
+            this.Editar.Name = "Editar";
+            this.Editar.Text = "Editar";
+            // 
+            // Eliminar
+            // 
+            this.Eliminar.HeaderText = "Eliminar";
+            this.Eliminar.Name = "Eliminar";
             // 
             // barrioText
             // 
             this.barrioText.BackColor = System.Drawing.SystemColors.ControlText;
             this.barrioText.Font = new System.Drawing.Font("Cambria", 11F);
             this.barrioText.ForeColor = System.Drawing.SystemColors.Menu;
-            this.barrioText.Location = new System.Drawing.Point(17, 120);
+            this.barrioText.Location = new System.Drawing.Point(279, 48);
             this.barrioText.Name = "barrioText";
             this.barrioText.Size = new System.Drawing.Size(137, 25);
             this.barrioText.TabIndex = 61;
             this.barrioText.Text = "Barrio";
-            // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.Color.Transparent;
-            this.button5.FlatAppearance.BorderSize = 2;
-            this.button5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button5.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Bold);
-            this.button5.ForeColor = System.Drawing.Color.White;
-            this.button5.Location = new System.Drawing.Point(470, 339);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(122, 31);
-            this.button5.TabIndex = 58;
-            this.button5.Text = "Eliminar";
-            this.button5.UseVisualStyleBackColor = false;
             // 
             // estrellasText
             // 
             this.estrellasText.BackColor = System.Drawing.SystemColors.ControlText;
             this.estrellasText.Font = new System.Drawing.Font("Cambria", 11F);
             this.estrellasText.ForeColor = System.Drawing.SystemColors.Menu;
-            this.estrellasText.Location = new System.Drawing.Point(17, 168);
+            this.estrellasText.Location = new System.Drawing.Point(17, 79);
             this.estrellasText.Name = "estrellasText";
             this.estrellasText.Size = new System.Drawing.Size(137, 25);
             this.estrellasText.TabIndex = 62;
             this.estrellasText.Text = "Estrellas";
-            // 
-            // button6
-            // 
-            this.button6.BackColor = System.Drawing.Color.Transparent;
-            this.button6.FlatAppearance.BorderSize = 2;
-            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
-            this.button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Bold);
-            this.button6.ForeColor = System.Drawing.Color.White;
-            this.button6.Location = new System.Drawing.Point(210, 339);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(122, 31);
-            this.button6.TabIndex = 57;
-            this.button6.Text = "Editar";
-            this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // tipoAlojamientoCombo
             // 
@@ -180,7 +207,7 @@ namespace Agencia.Views
             this.tipoAlojamientoCombo.Items.AddRange(new object[] {
             "Hotel",
             "Cabaña"});
-            this.tipoAlojamientoCombo.Location = new System.Drawing.Point(18, 216);
+            this.tipoAlojamientoCombo.Location = new System.Drawing.Point(17, 3);
             this.tipoAlojamientoCombo.Name = "tipoAlojamientoCombo";
             this.tipoAlojamientoCombo.Size = new System.Drawing.Size(136, 25);
             this.tipoAlojamientoCombo.TabIndex = 63;
@@ -196,7 +223,7 @@ namespace Agencia.Views
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Tahoma", 11F, System.Drawing.FontStyle.Bold);
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(17, 339);
+            this.button4.Location = new System.Drawing.Point(17, 173);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(122, 31);
             this.button4.TabIndex = 60;
@@ -209,7 +236,7 @@ namespace Agencia.Views
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(14, 21);
+            this.label1.Location = new System.Drawing.Point(230, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(186, 25);
             this.label1.TabIndex = 64;
@@ -224,9 +251,7 @@ namespace Agencia.Views
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.button4);
             this.panel1.Controls.Add(this.tipoAlojamientoCombo);
-            this.panel1.Controls.Add(this.button6);
             this.panel1.Controls.Add(this.estrellasText);
-            this.panel1.Controls.Add(this.button5);
             this.panel1.Controls.Add(this.barrioText);
             this.panel1.Controls.Add(this.alojamientosGrid);
             this.panel1.Controls.Add(this.ciudadText);
@@ -243,9 +268,9 @@ namespace Agencia.Views
             this.baniosText.BackColor = System.Drawing.SystemColors.ControlText;
             this.baniosText.Font = new System.Drawing.Font("Cambria", 11F);
             this.baniosText.ForeColor = System.Drawing.SystemColors.Menu;
-            this.baniosText.Location = new System.Drawing.Point(163, 216);
+            this.baniosText.Location = new System.Drawing.Point(163, 119);
             this.baniosText.Name = "baniosText";
-            this.baniosText.Size = new System.Drawing.Size(137, 25);
+            this.baniosText.Size = new System.Drawing.Size(108, 25);
             this.baniosText.TabIndex = 68;
             this.baniosText.Text = "N° de baños";
             // 
@@ -254,7 +279,7 @@ namespace Agencia.Views
             this.habitacionesText.BackColor = System.Drawing.SystemColors.ControlText;
             this.habitacionesText.Font = new System.Drawing.Font("Cambria", 11F);
             this.habitacionesText.ForeColor = System.Drawing.SystemColors.Menu;
-            this.habitacionesText.Location = new System.Drawing.Point(163, 168);
+            this.habitacionesText.Location = new System.Drawing.Point(17, 118);
             this.habitacionesText.Name = "habitacionesText";
             this.habitacionesText.Size = new System.Drawing.Size(137, 25);
             this.habitacionesText.TabIndex = 67;
@@ -265,9 +290,9 @@ namespace Agencia.Views
             this.precioText.BackColor = System.Drawing.SystemColors.ControlText;
             this.precioText.Font = new System.Drawing.Font("Cambria", 11F);
             this.precioText.ForeColor = System.Drawing.SystemColors.Menu;
-            this.precioText.Location = new System.Drawing.Point(163, 120);
+            this.precioText.Location = new System.Drawing.Point(166, 79);
             this.precioText.Name = "precioText";
-            this.precioText.Size = new System.Drawing.Size(137, 25);
+            this.precioText.Size = new System.Drawing.Size(105, 25);
             this.precioText.TabIndex = 66;
             this.precioText.Text = "Precio por dia";
             // 
@@ -276,18 +301,11 @@ namespace Agencia.Views
             this.personasText.BackColor = System.Drawing.SystemColors.ControlText;
             this.personasText.Font = new System.Drawing.Font("Cambria", 11F);
             this.personasText.ForeColor = System.Drawing.SystemColors.Menu;
-            this.personasText.Location = new System.Drawing.Point(163, 73);
+            this.personasText.Location = new System.Drawing.Point(163, 48);
             this.personasText.Name = "personasText";
-            this.personasText.Size = new System.Drawing.Size(137, 25);
+            this.personasText.Size = new System.Drawing.Size(105, 25);
             this.personasText.TabIndex = 65;
             this.personasText.Text = "N° de personas";
-            // 
-            // Index
-            // 
-            this.Index.HeaderText = "Index";
-            this.Index.Name = "Index";
-            this.Index.ReadOnly = true;
-            this.Index.Width = 230;
             // 
             // AdmAlojamientos
             // 
@@ -319,9 +337,7 @@ namespace Agencia.Views
         private System.Windows.Forms.TextBox ciudadText;
         private System.Windows.Forms.DataGridView alojamientosGrid;
         private System.Windows.Forms.TextBox barrioText;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.TextBox estrellasText;
-        private System.Windows.Forms.Button button6;
         private System.Windows.Forms.ComboBox tipoAlojamientoCombo;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
@@ -330,6 +346,13 @@ namespace Agencia.Views
         private System.Windows.Forms.TextBox personasText;
         private System.Windows.Forms.TextBox habitacionesText;
         private System.Windows.Forms.TextBox baniosText;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tipo_text;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Barrio_text;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NroHabitacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ciudad_text;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Estrellas_text;
+        private System.Windows.Forms.DataGridViewButtonColumn Editar;
+        private System.Windows.Forms.DataGridViewButtonColumn Eliminar;
     }
 }
